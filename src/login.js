28cec3100607aac883a393adc59b703c68e9a8ac
Naveed Qamar <60569200+naveedqamar650@users.noreply.github.com>
2022-12-08 17:43:14 +0500

    Updated Login

--- src/login.js
+++ src/login.js
@@ -1,61 +1,12 @@
 import React from 'react';
 import {View, Text, Touchable, TouchableOpacity, TextInput} from 'react-native';
+import { YellowBox } from 'react-native-web';
 import Background from './background';
 
 const Login = (props) => {
   return (
     <Background>
-      <View style={{alignItems: 'center', width: 460}}>
-        <Text
-          style={{
-            color: 'white',
-            fontSize: 64,
-            fontWeight: 'bold',
-            marginVertical: 20,
-          }}>
-          Login
-        </Text>
-        <View
-          style={{
-            backgroundColor: 'white',
-            height: 700,
-            width: 460,
-            borderTopLeftRadius: 130,
-            paddingTop: 100,
-            alignItems: 'center',
-          }}>
-          <Text style={{fontSize: 40, color: '#162e3e', fontWeight: 'bold'}}>
-            Welcome Back
-          </Text>
-          <Text
-            style={{
-              color: 'grey',
-              fontSize: 19,
-              fontWeight: 'bold',
-              marginBottom: 20,
-            }}>
-            Login to your account
-          </Text>
-          <Field
-            placeholder="Email / Username"
-            keyboardType={'email-address'}
-          />
-          <Field placeholder="Password" secureTextEntry={true} />
-          <View
-            style={{alignItems: 'flex-end', width: '78%', paddingRight: 16, marginBottom: 200}}>
-            <Text style={{color: '#162e3e', fontWeight: 'bold', fontSize: 16}}>
-              Forgot Password ?
-            </Text>
-          </View>
-          <Btn textColor='white' bgColor={'#162e3e'} btnLabel="Login" Press={() => alert("Logged In")} />
-          <View style={{ display: 'flex', flexDirection :'row', justifyContent: "center" }}>
-            <Text style={{ fontSize: 16, fontWeight:"bold" }}>Don't have an account ? </Text>
-            <TouchableOpacity onPress={() => props.navigation.navigate("Signup")}>
-            <Text style={{ color: '#162e3e', fontWeight: 'bold', fontSize: 16 }}>Signup</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
+      
     </Background>
   );
 };
