e5ae7a7fad550bd1b4112949cd64f40a973159e3
Naveed Qamar <60569200+naveedqamar650@users.noreply.github.com>
2022-12-08 12:45:54 +0500

    Socket Created

---
+++ socket/index.js
@@ -0,0 +1,22 @@
+// By using the documentation of Socket.io
+//https://socket.io/get-started/chat
+
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+//const io = require("socket.io").listen(server);
+// having error with listen function so removing it
+const io = require("socket.io")(server);
+const port = 3000;
+
+io.on("connection", socket => {
+  console.log("User Connected");
+  socket.on("Message : ", message => {s
+    console.log(message);
+    io.emit("Message : ", message);
+  });
+});
+
+server.listen(
+    port, () => console.log("Server Running Port Number : " + port)
+    );
